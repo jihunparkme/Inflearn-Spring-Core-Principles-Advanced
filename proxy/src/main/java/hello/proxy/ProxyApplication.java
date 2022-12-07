f6df360609144e82f90cb6242c05bc375fd7d131
Jihun Park <jihunpark.tech@gmail.com>
2022-12-07 22:28:13 +0900

    [Proxy Pattern & Decorator Pattern] 예제 프로젝트 만들기 v2

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -1,11 +1,12 @@
 package hello.proxy;
 
 import hello.proxy.config.AppV1Config;
+import hello.proxy.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class) // 클래스를 스프링 빈으로 등록
+@Import({AppV1Config.class, AppV2Config.class}) // 클래스를 스프링 빈으로 등록
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") // 컴포넌트 스캔 시작 위치 지정 (=@ComponentScan)
 public class ProxyApplication {
 

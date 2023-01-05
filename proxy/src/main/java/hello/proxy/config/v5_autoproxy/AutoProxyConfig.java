690ac3d59d3c8f8ba708ac25e117e57010bbcd45
Jihun Park <jihunpark.tech@gmail.com>
2023-01-05 16:49:44 +0900

    [빈 후처리기] 스프링이 제공하는 빈 후처리기2

--- proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ proxy/src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -17,10 +18,40 @@ public class AutoProxyConfig {
 
     // 스프링은 자동 프록시 생성기 빈 후처리기(AnnotationAwareAspectJAutoProxyCreator)를 자동으로 등록
 
-    @Bean
+//    @Bean
     public Advisor advisor1(LogTrace logTrace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        //advisor = pointcut + advice
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    /** package 기준 포인트컷 적용
+     * AspectJExpressionPointcut : AspectJ 포인트컷 표현식 적용
+     * execution(* hello.proxy.app..*(..)) : AspectJ가 제공하는 포인트컷 표현식
+     *      * : 모든 반환 타입
+     *      hello.proxy.app.. : 해당 패키지와 그 하위 패키지
+     *      *(..) : * 모든 메서드 이름, (..) 파라미터는 상관 없음
+     * -> hello.proxy.app 패키지와 그 하위 패키지의 모든 메서드는 포인트컷의 매칭 대상
+     */
+    @Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        //advisor = pointcut + advice
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    /** method 기준 포인트컷 적용
+     * hello.proxy.app 패키지와 하위 패키지의 모든 메서드는 포인트컷의 매칭하되,
+     * noLog() 메서드는 제외
+     */
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         //advisor = pointcut + advice
         return new DefaultPointcutAdvisor(pointcut, advice);

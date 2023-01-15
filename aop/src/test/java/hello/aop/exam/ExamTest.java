75efa570f2a14e8273ab769e3d384ae5775fc0a8
Jihun Park <jihunpark.tech@gmail.com>
2023-01-15 23:07:42 +0900

    [스프링 AOP - 실전 예제] 로그 출력 AOP

--- aop/src/test/java/hello/aop/exam/ExamTest.java
+++ aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,15 +1,34 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.TraceAspect;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
+@Import(TraceAspect.class)
 @SpringBootTest
 public class ExamTest {
 
     @Autowired
     ExamService examService;
 
+    /**
+     * [trace] void hello.aop.exam.ExamService.request(String) args=[data0]
+     * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data0]
+     *
+     * [trace] void hello.aop.exam.ExamService.request(String) args=[data1]
+     * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data1]
+     *
+     * [trace] void hello.aop.exam.ExamService.request(String) args=[data2]
+     * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data2]
+     *
+     * [trace] void hello.aop.exam.ExamService.request(String) args=[data3]
+     * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data3]
+     *
+     * [trace] void hello.aop.exam.ExamService.request(String) args=[data4]
+     * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data4]
+     */
     @Test
     void test() {
         for (int i = 0; i < 5; i++) {

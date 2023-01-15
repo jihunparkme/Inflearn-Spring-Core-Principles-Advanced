01a8498b904f1501ea5b4e3d81f259dade214d8b
Jihun Park <jihunpark.tech@gmail.com>
2023-01-15 23:22:34 +0900

    [스프링 AOP - 실전 예제] 재시도 AOP

--- aop/src/test/java/hello/aop/exam/ExamTest.java
+++ aop/src/test/java/hello/aop/exam/ExamTest.java
@@ -1,12 +1,13 @@
 package hello.aop.exam;
 
+import hello.aop.exam.aop.RetryAspect;
 import hello.aop.exam.aop.TraceAspect;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TraceAspect.class)
+@Import({TraceAspect.class, RetryAspect.class})
 @SpringBootTest
 public class ExamTest {
 
@@ -16,18 +17,30 @@ public class ExamTest {
     /**
      * [trace] void hello.aop.exam.ExamService.request(String) args=[data0]
      * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data0]
+     * [retry] String hello.aop.exam.ExamRepository.save(String) retry=@hello.aop.exam.annotation.Retry(4)
+     * [retry] try count=1/4
      *
      * [trace] void hello.aop.exam.ExamService.request(String) args=[data1]
      * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data1]
+     * [retry] String hello.aop.exam.ExamRepository.save(String) retry=@hello.aop.exam.annotation.Retry(4)
+     * [retry] try count=1/4
      *
      * [trace] void hello.aop.exam.ExamService.request(String) args=[data2]
      * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data2]
+     * [retry] String hello.aop.exam.ExamRepository.save(String) retry=@hello.aop.exam.annotation.Retry(4)
+     * [retry] try count=1/4
      *
      * [trace] void hello.aop.exam.ExamService.request(String) args=[data3]
      * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data3]
+     * [retry] String hello.aop.exam.ExamRepository.save(String) retry=@hello.aop.exam.annotation.Retry(4)
+     * [retry] try count=1/4
      *
      * [trace] void hello.aop.exam.ExamService.request(String) args=[data4]
      * [trace] String hello.aop.exam.ExamRepository.save(String) args=[data4]
+     * [retry] String hello.aop.exam.ExamRepository.save(String) retry=@hello.aop.exam.annotation.Retry(4)
+     * [retry] try count=1/4
+     *
+     * [retry] try count=2/4 -> retry 시도
      */
     @Test
     void test() {
